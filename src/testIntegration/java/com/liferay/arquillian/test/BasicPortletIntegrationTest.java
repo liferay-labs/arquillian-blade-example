0baa311e8914b2348dd88ef23af4bcdeb46eb06d
Jim Hinkey <james.hinkey@liferay.com>
2017-05-11 13:58:46 -0400

    LRDOCS-3661 On Windows use gradlew.bat script instead of gradlew script

--- src/testIntegration/java/com/liferay/arquillian/test/BasicPortletIntegrationTest.java
+++ src/testIntegration/java/com/liferay/arquillian/test/BasicPortletIntegrationTest.java
@@ -42,8 +42,15 @@ public class BasicPortletIntegrationTest {
 	public static JavaArchive create() throws Exception {
 		final File tempDir = Files.createTempDir();
 
+		String gradlew = "./gradlew";
+
+		String osName = System.getProperty("os.name", "");
+		if (osName.toLowerCase().contains("windows")) {
+			gradlew = "./gradlew.bat";
+		}
+
 		final ProcessBuilder processBuilder = new ProcessBuilder(
-			"./gradlew", "jar", "-Pdir=" + tempDir.getAbsolutePath());
+			gradlew, "jar", "-Pdir=" + tempDir.getAbsolutePath());
 
 		final Process process = processBuilder.start();
 
